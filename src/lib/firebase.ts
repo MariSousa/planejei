18527214ebfc586410de32cabff96b2a913c4d8e
Mariana de Sousa <59418548+MariSousa@users.noreply.github.com>
2025-08-11 21:07:23 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,24 +3,28 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6vY4_rKyYRNMJEEFXs12QpP61yCLw9Fk",
-  authDomain: "vida-financeira-planner.firebaseapp.com",
-  projectId: "vida-financeira-planner",
-  storageBucket: "vida-financeira-planner.appspot.com",
-  messagingSenderId: "84532001329",
-  appId: "1:84532001329:web:a672d3eb98dcf99ff4b310",
-  measurementId: "G-XY94WQRR7Z"
+  "projectId": "planejei-4oo42",
+  "appId": "1:120489705522:web:660ffb2c96e9c53e60fc2b",
+  "databaseURL": "https://planejei-4oo42-default-rtdb.firebaseio.com",
+  "storageBucket": "planejei-4oo42.appspot.com",
+  "apiKey": "AIzaSyBx4kriOPiZBPacBVy-kSxcvRBnQuw0euM",
+  "authDomain": "planejei-4oo42.firebaseapp.com",
+  "messagingSenderId": "120489705522",
+  "measurementId": "G-DR2P9P32B6"
 };
 
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const rtdb = getDatabase(app);
 
 // Connect to emulators if running locally in dev mode
 if (typeof window !== 'undefined' && window.location.hostname === 'localhost' && process.env.NODE_ENV === 'development') {
@@ -28,6 +32,7 @@ if (typeof window !== 'undefined' && window.location.hostname === 'localhost' &&
     // Point to the emulators for auth and firestore
     connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
     connectFirestoreEmulator(db, 'localhost', 8080);
+    connectDatabaseEmulator(rtdb, 'localhost', 9000);
 }
 
 
@@ -41,4 +46,4 @@ if (typeof window !== 'undefined') {
 }
 
 
-export { app, auth, db };
+export { app, auth, db, rtdb };
